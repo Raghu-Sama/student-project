dcf008f5849b9ccb22598f0348cd1fecfc08f30f
SAMA RAGHU <raghu.sama@smartims.com>
2024-07-21 15:50:09 +0530

    Initial commit of student project of Bouali ali

---
+++ src/test/java/com/complex/student/StudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.complex.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
